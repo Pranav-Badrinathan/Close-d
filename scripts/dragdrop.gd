291527ef61b81a5d9479261f9def0ac7448d6d4c
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2022-10-01 16:28:00 -0400

    Tool return Implemented
    Removed Kinematic body 2D and replaced with Area 2D, and set it to go back to start pos when it is let go of.

--- scripts/dragdrop.gd
+++ scripts/dragdrop.gd
@@ -2,29 +2,29 @@ extends Node2D
 
 var dragging = false
 var pos_from_centre = Vector2()
-var _velocity = Vector2()
+var start_pos = Vector2()
 
 enum Tool {SCREWDRIVER, WRENCH, SLEDGEHAMMER, PLIERS, AXE}
 export (Tool) var _tool
 
 export (Texture) var image
-export var fall_speed = 0
 
 func _ready():
 	get_node("Texture").set_texture(image)
+	start_pos = position
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
 	if event.is_action_pressed("LClick"):
 		pos_from_centre = get_global_mouse_position() - position
 		dragging = true
 		get_tree().set_input_as_handled()
-	print("JHEDYFFFDMLAXS")
 
-func _physics_process(_delta):
-	if dragging: position = get_global_mouse_position() - pos_from_centre
-
+func _physics_process(delta):
+	if dragging:
+		position = get_global_mouse_position() - pos_from_centre
 
 func _input(event):
 	if event.is_action_released("LClick"): 
 		dragging = false
 		pos_from_centre = Vector2()
+		position = start_pos
