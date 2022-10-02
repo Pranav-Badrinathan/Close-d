6dff2e172aa5b3edb11daf4251dcacdbc1556ffd
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2022-10-02 13:38:49 -0400

    music

--- scripts/MachineAudio.gd
+++ scripts/MachineAudio.gd
@@ -1,4 +1,20 @@
 extends Node2D
 
+export (AudioStream) var break_
+export (AudioStream) var music_
+
+onready var audio = get_node(".")
+
+func _ready():
+	audio.set_bus("NewBus")
+
 func _play():
-	get_node(".").play()
+	audio.stop()
+	audio.stream = break_
+	audio.play()
+
+func _play2():
+	if audio.stream == null:
+		audio.stop()
+		audio.stream = music_
+		audio.play()
