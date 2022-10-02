ea5a9dc0fbf29309c692810ef6531fb5f74777a6
Leif Clark <59668580+leifekstromclark@users.noreply.github.com>
2022-10-02 12:05:41 -0400

    Door fall

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
-enum State {OPEN, CLOSING, OPENING}
+enum State {OPEN, CLOSING, OPENING, FALLING}
 
 onready var door = get_node("Door")
-onready var light = get_node("Machine/Light")
+onready var light = get_node("Machine/MachineSprite/Light")
 onready var time_readout = get_node("TimeReadout")
+onready var machine = get_node("Machine")
 
 var door_state = State.OPEN
 var door_progress = 0
 var door_interval
 var peak_progress = 1
 var time_until_open = 10
+var time_until_fall = 1
+
 
 export var door_speed = 10
 export (Texture) var light_on
@@ -18,6 +21,8 @@ export (Texture) var light_off
 
 var open_speed = 1
 var close_speed = 0.09
+var falling_speed = 0
+var falling_acceleration = 5
 
 var first_closing = true
 
@@ -36,6 +41,16 @@ func _process(d):
 		if door_state == State.CLOSING:
 			start_opening()
 		time_until_open = 10
+		
+	elif door_state == State.FALLING:
+		time_until_fall -= d
+		if time_until_fall <= 0:
+			door_progress += falling_speed * d +  pow(falling_acceleration * d, 2) / 2
+			falling_speed += falling_acceleration * d
+			if door_progress >= 1:
+				door_progress = 1
+				#you win
+			door.position.y = door_interval[0] + (door_interval[1] - door_interval[0]) * door_progress
 		
 	elif door_state == State.CLOSING:
 		door_progress += close_speed * d
@@ -72,3 +87,10 @@ func _on_OpenButton_pressed():
 		if first_closing:
 			start_timer()
 		start_closing()
+		break_machine()
+		
+func break_machine():
+	machine.gravity_scale = 50
+	light.texture = light_off
+	door_state = State.FALLING
+	time_readout.visible = false
